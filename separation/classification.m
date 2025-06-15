b2abaf961c47b1169401517ce8680359110f802d
ivanachrtkova <153823032+ivanachrtkova@users.noreply.github.com>
2025-06-15 20:35:27 +0200

    Update classification.m

--- separation/classification.m
+++ separation/classification.m
@@ -1,9 +1,16 @@
 
 function [mapping, idx] = classification(features, opts)
-    [mapping, ~] = fast_tsne(features, opts); % dimred via Flt-SNE package
+% Function for classification of EOD features
+% Input  - features: extracted EOD features
+%        - opts: options for t-SNE (e.g., perplexity, ...)
+% Output - mapping: FIt-SNE output
+%        - idx: corresponding labels from hierarchical clustering
+
+    % dimensionality reduction via Flt-SNE package
+    [mapping, ~] = fast_tsne(features, opts);
 
     % classification via hierarchical clustering
     Y = pdist(mapping);
-    Z = linkage(Y, "average");
-    idx = cluster(Z,"maxclust", 2); 
+    Z = linkage(Y, "average"); % we are using average linkage 
+    idx = cluster(Z,"maxclust", 2); % clustering into two clusters (two individuals)
 end
